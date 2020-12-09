8f7c1890e609100deac6fc04b33d3929aef62cef
nfangxu <nfangxu@gmail.com>
2020-12-09 14:58:54 +0800

    feat: add app/helpers.php

---
+++ app/helpers.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Fangx's Packages
+ *
+ * @link     https://nfangxu.com
+ * @document https://pkg.nfangxu.com
+ * @contact  nfangxu@gmail.com
+ * @author   nfangxu
+ * @license  https://pkg.nfangxu.com/license
+ */
+
+if (function_exists('assets')) {
+    function assets($path): string
+    {
+        return sprintf('/assets/%s', $path);
+    }
+}
